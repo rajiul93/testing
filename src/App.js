49cf7b3fc4b94c72f5ada38135e133a975be805e
rajiul93 <rajiulislam283@gmail.com>
2023-04-15 07:33:27 +0600

    add all function and all about

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Home from './component/Home/Home';
+import Complete from './component/Running/Complete';
+import Running from './component/Running/Running';
 
 function App() {
   return (
-    <div className="App">
-    <h1>hellow world update now</h1>
-    </div>
-  );
+
+    <Routes  >
+      <Route path='/' element={<Home />} />
+      <Route path='/running' element={<Running />} />
+      <Route path='/complete' element={<Complete />} />
+    </Routes>
+  )
 }
 
 export default App;
